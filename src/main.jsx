196a6ae94a5abda74d44b75310662e8e0a4d7fb5
andre-baiocchi <andre.baiocchi@trustly.com>
2025-05-19 23:28:31 -0400

    Implementação de tela de login

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.jsx';
-import Login from './Login.jsx';
-import Register from './Register.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
